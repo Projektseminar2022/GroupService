8506c8e38a212b3d0d28dc8ad57e82252427e43b
Tim <82676248+Tim-53@users.noreply.github.com>
2023-01-05 00:56:34 +0100

    user as document

--- src/main/java/de/GroupService/model/User.java
+++ src/main/java/de/GroupService/model/User.java
@@ -4,22 +4,31 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.relational.core.mapping.Table;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
-@Table("users")
+//@Table("users")
+@Document
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
     @Id
     private UUID id;
+	private ArrayList<UUID> groups;
+	private Location location;
+    private String name;
 
-    public static final String sqlCreate = """
-			CREATE TABLE IF NOT EXISTS users (
-				id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-																)
-			""";
+
+
+
+//    public static final String sqlCreate = """
+//			CREATE TABLE IF NOT EXISTS users (
+//				id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+//																)
+//			""";
 
 }
