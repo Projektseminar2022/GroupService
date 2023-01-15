659ad03a4faa6c68e32a86b04da5493e6e7576f4
oolbrueck <oligo99@t-online.de>
2023-01-15 13:58:05 +0100

    feign

--- src/main/java/de/GroupService/components/CallNotificationService.java
+++ src/main/java/de/GroupService/components/CallNotificationService.java
@@ -1,27 +1,30 @@
 package de.GroupService.components;
 
 import de.GroupService.components.client.NotificationClient;
+import de.GroupService.dto.NotificationDTO;
 import feign.Feign;
 import feign.gson.GsonDecoder;
 import feign.gson.GsonEncoder;
 import feign.okhttp.OkHttpClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.UUID;
 
 @Service
 public class CallNotificationService {
-    private final NotificationClient weatherDataClient;
+
+    private final NotificationClient notificationClient;
     public CallNotificationService() {
-            this.weatherDataClient = Feign.builder()
-                    .client(new OkHttpClient())
-                    .encoder(new GsonEncoder())
-                    .decoder(new GsonDecoder())
-                    .target(NotificationClient.class, "http://localhost:8081/api/books"); //todo add real url
-        }
+        this.notificationClient = Feign.builder()
+                .client(new OkHttpClient())
+                .encoder(new GsonEncoder())
+                .decoder(new GsonDecoder())
+                .target(NotificationClient.class, "http://localhost:8081/api/books");
+    }
 
     public boolean callNotification(String message, UUID userId) {
         try {
-            weatherDataClient.sendPrivateNotification(message, userId.toString());
+            notificationClient.notifyUser(new NotificationDTO(message, userId.toString()));
             return true;
         } catch (Exception e) {
             return false;
--- src/main/java/de/GroupService/components/client/NotificationClient.java
+++ src/main/java/de/GroupService/components/client/NotificationClient.java
@@ -1,10 +1,12 @@
 package de.GroupService.components.client;
 
+import de.GroupService.dto.NotificationDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-import feign.Param;
-import feign.RequestLine;
-
+@FeignClient(name = "NotificationClient", url = "localhost:1234")
 public interface NotificationClient {
-        @RequestLine("POST /notify/private/")
-        void sendPrivateNotification(@Param("userLocationDTO") String message, String recievingUser);
+    @PostMapping("/*/private")
+    void notifyUser(@RequestBody NotificationDTO notificationDTO);
 }
